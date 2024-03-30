2b96062dc9684a976f3484d2b81418f43488d041
Moon_Star32 <azamat.yusupov.04@outlook.com>
2024-03-30 04:18:21 +0300

    add app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/to_be_continued.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
